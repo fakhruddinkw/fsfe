d9fba52f728c320526d570daa516fc60de3b2b6b
FMK2312 <fmkhandwala39@gmail.com>
2023-09-19 15:13:18 +0530

    adding db logging

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,15 @@ server.listen(3000, function () {
   console.log("server started on port: 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin websocket */
 const WebSocketServer = require("ws").Server;
 
@@ -27,6 +36,12 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server!");
   }
 
+  db.run(`
+          INSERT INTO visitors (count, time)
+          VALUES (${numOfClients}, datetime('now'))
+  
+  `);
+
   ws.on("close", function close() {
     console.log("closing connection");
   });
@@ -37,3 +52,29 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+/** Database begin */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+      CREATE TABLE visitors (
+
+        count INTEGER,
+        time TEXT
+      )
+  `);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down DB");
+  db.close();
+}
